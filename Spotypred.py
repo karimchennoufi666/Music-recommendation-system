beab05f81df8592bf90b76159403c1c99d4e7876
karimchennoufi666 <karimchennoufi1@gmail.com>
2025-10-03 12:34:23 +0100

    Update Spotypred.py

--- Spotypred.py
+++ Spotypred.py
@@ -19,7 +19,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
 
 # ================== Load your datasets ==================
 # Replace paths with your actual CSV files
-data = pd.read_csv(r"C:\Users\karim\Spotify\data (2).csv")
+data = pd.read_csv(r"C:/Users/karim/Spotify/data (2).csv")
 spotify_tracks = pd.read_csv("spotify_tracks.csv")
 
 # ================== Functions ==================
